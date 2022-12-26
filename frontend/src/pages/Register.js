00c00a6b90e674895db8c5e6b847c10c4552b3e0
danangg67 <dananghadi67@student.uns.ac.id>
2022-12-26 13:53:48 +0700

    Initial commit

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,159 @@
+//import hook react
+import React, { useState } from "react";
+
+//import hook useHitory from react router dom
+import { useHistory } from "react-router";
+
+//import axios
+import axios from "axios";
+
+function Register() {
+  //define state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("admin");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  //define state validation
+  const [validation, setValidation] = useState([]);
+
+  //define history
+  const history = useHistory();
+
+  //function "registerHanlder"
+  const registerHandler = async (e) => {
+    e.preventDefault();
+
+    //initialize formData
+    const formData = new FormData();
+
+    //append data to formData
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("role", role);
+    formData.append("password", password);
+    formData.append("password_confirmation", passwordConfirmation);
+
+    //send data to server
+    await axios
+      .post("http://localhost:8000/api/register", formData)
+      .then(() => {
+        //redirect to logi page
+        history.push("/");
+      })
+      .catch((error) => {
+        //assign error to state "validation"
+        setValidation(error.response.data);
+      });
+  };
+
+  return (
+    <div className="container" style={{ marginTop: "120px" }}>
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card border-0 rounded shadow-sm">
+            <div className="card-body">
+              <center>
+                <h3 className="fw-bold">HALAMAN REGISTER</h3>
+              </center>
+              <hr />
+              <form onSubmit={registerHandler}>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">Nama Lengkap</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Masukkan Nama Lengkap"
+                      />
+                    </div>
+                    {validation.name && (
+                      <div className="alert alert-danger">
+                        {validation.name[0]}
+                      </div>
+                    )}
+                  </div>
+                  <div className="col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">E-mail</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Masukkan Alamat Email"
+                      />
+                    </div>
+                    {validation.email && (
+                      <div className="alert alert-danger">
+                        {validation.email[0]}
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">Password</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Masukkan Password"
+                      />
+                    </div>
+                    {validation.password && (
+                      <div className="alert alert-danger">
+                        {validation.password[0]}
+                      </div>
+                    )}
+                  </div>
+                  <div className="col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">Konfirmasi Password</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        value={passwordConfirmation}
+                        onChange={(e) =>
+                          setPasswordConfirmation(e.target.value)
+                        }
+                        placeholder="Masukkan Konfirmasi Password"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label">Role</label>
+                      <select
+                        className="form-select"
+                        required
+                        onChange={(e) => setRole(e.target.value)}
+                      >
+                        <option value="admin">Admin</option>
+                        <option value="user">User</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Registrasi
+                </button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
